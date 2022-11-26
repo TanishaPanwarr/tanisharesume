a2f04a50140ed13fd5f843e38f45d18ac098358a
TanishaPanwarr <tanishapanwar980@gmail.com>
2022-11-27 05:21:24 +0530

    first commit

--- tanuresume/settings.py
+++ tanuresume/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')iu$rji)j63b#*tu4fi8y)q!t-3d3x#)j+jab9(i!!=bzk0+hx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
